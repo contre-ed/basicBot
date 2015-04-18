a44027043ffa9727e818e260d5c1fa45b4566017
contre-ed <augurey@gmail.com>
2015-04-18 00:07:07 -0400

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2648,6 +2648,33 @@
                 }
             },
 
+            zujoCommand: {
+                command: 'zujo',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.yooo)
+                    }
+                }
+            },
+            
+            andrewCommand: {
+                command: 'andrew',
+                rank: 'user',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    else {
+                        API.sendChat(basicBot.chat.number)
+                    }
+                }
+            },
+
+
 
             purchaseCommand: {
                 command: ['purchase'],
